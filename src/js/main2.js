4386adc334d0d6b9de49074a5e0f7266fb20a329
Andrew Collier <andrew@acollier.com>
2014-11-23 23:20:51 -0500

    remove unneeded code and distance dependencies

--- src/js/main2.js
+++ src/js/main2.js
@@ -161,116 +161,12 @@ var renderers = {
     annotator.add(1412039701, "You are here");
     annotator.update();
     */
-  },
-
-  addSegmentLabel: function(roadFeature, transformation) {
-    var transformations = []
-    transformations.push(transformation);
-
-    // Remove labels from paths in prior generations
-    d3.selectAll('.segment-label').remove();
-
-    // Add labels to newly translated paths
-    var texts = roadFeature.selectAll("text").data(transformations).enter();
-    texts.append("text")
-    .text(function(d, i) {
-      if (typeof d === 'undefined' || d.name === 'undefined') {
-        return '';
-      } else {
-        return d.name;
-      }
-
-    })
-    .attr('class', 'segment-label')
-    .attr('x', function(d, i) {
-      return d.boundaries.x1 + 10;
-    })
-    .attr('y', function(d, i) {
-      return d.boundaries.y1 - 10;
-    })
-
-  },
-
-  spotlightSegments: function(activeSegment, roadFeature) {
-    if (activeSegment.className.baseVal.indexOf('segment-flyout') === -1) {
-    //if (!activeSegment.classList.contains('segment-flyout')) {
-
-      var spotlightRadius = 5;
-      var priorTransformations = [];
-
-      // Remove previous generation segment flyouts, if any
-      d3.selectAll('.segment-flyout').classed('segment-flyout',false)
-
-      // Calculate the center of mass for all segments within the spotlightRadius
-      var com = rendererHelpers.getCentroid(activeSegment, roadFeature, spotlightRadius)
-
-      // See the transform attribute on the road segments
-      var activeBoundaries = rendererHelpers.getBoundaries(activeSegment);
-      var activeBoundariesWithMargin = rendererHelpers.addMargin(activeBoundaries, spotlightRadius);
-      d3.selectAll('.segment')
-        .transition()
-        .duration(750)
-        .attr('transform', function(d, i) {
-          var currentBoundaries = rendererHelpers.getBoundaries(this);
-
-          // Determine if current segment is close to the active segment
-          currentSegmentMidpoint = this.getPointAtLength(this.getTotalLength()/2);
-          var epicenterDistance = rendererHelpers.calculateDistance(com, currentSegmentMidpoint);
-          if (activeSegment.id === this.id) {
-            d3.select(this).classed('segment-nonflyout',false);
-            d3.select(this).classed('segment-flyout',true);
-            d3.select(this).attr('marker-end','url(#segment-direction)');
-
-            // Render label on mouseover
-            d3.select(this).on('mouseover', function(d, i) {
-              renderers.addSegmentLabel(roadFeature,priorTransformation);
-            })
-
-          } else if (activeSegment.id !== this.id && rendererHelpers.hasOverlap(activeBoundariesWithMargin, currentBoundaries)) {
-            //console.log(this.id);
-            var offsetVector = rendererHelpers.getOffsetVector(this, priorTransformations, currentSegmentMidpoint, com, currentBoundaries);
-            //console.log("     ");
-
-            // Store a record of this transformation
-            var currentSegmentEnd = this.getPointAtLength(this.getTotalLength());
-            var priorTransformation = {
-              'name': d3.select(this).attr('data-title'),
-              'boundaries': rendererHelpers.translateBoundaries(currentBoundaries,offsetVector)
-
-            }
-            priorTransformations.push(priorTransformation);
-
-            // Set Flyout Formatting
-            d3.select(this).classed('segment-nonflyout',false);
-            d3.select(this).classed('segment-flyout',true);
-            if (typeof activeSegment.classList !== 'undefined') {
-              d3.select(this).attr('marker-end','url(#segment-direction)');
-            }
-
-            // Render label on mouseover
-            d3.select(this).on('mouseover', function(d, i) {
-              renderers.addSegmentLabel(roadFeature,priorTransformation);
-            })
-
-            // Return the Translation
-            return 'translate('+offsetVector.x+','+offsetVector.y+')';
-
-          // Return a zero translation if the current segment is not close to the active segment
-          } else {
-            d3.select(this).classed('segment-nonflyout',true);
-            d3.select(this).attr('marker-end','');
-            return 'translate(0,0)';
-          }
-        }
-      );
-
-    }
-
   }
 
 }
 
 var helper = {
+
   // Get a text description of the current congestion
   getCongestionRatioText: function(pairDatum) {
     var congestionRatio = pairDatum.speed/pairDatum.freeFlow;
@@ -288,11 +184,11 @@ var helper = {
     var seriesElement = {};
     var data = [];
     var formattedDatum;
-	if (utc === true) {
-		var currentTime = new Date(start+"-05:00");
-	} else {
-	  var currentTime = new Date(start);
-	}
+  	if (utc === true) {
+  		var currentTime = new Date(start+"-05:00");
+  	} else {
+  	  var currentTime = new Date(start);
+  	}
     var currentTimeSeconds
     currentTimeSeconds = currentTime.getTime()/1000;
 
@@ -349,10 +245,6 @@ var helper = {
     return seriesElement;
   },
 
-  getActivePercentileTab: function() {
-    return $('.percentile-graphs').children('.active').attr('id');
-  },
-
   getDayOfWeek: function() {
     now = new Date();
     return now.getDay();
@@ -400,186 +292,6 @@ var helper = {
       'freeFlow': Math.round(freeflowSummation/count),
       'impactedSegments': impactedSegments
     }
-  }
-}
-
-var rendererHelpers = {
-
-  calculateDistance: function (point1, point2) {
-    return Math.sqrt(Math.pow(point1.x-point2.x,2) + Math.pow(point1.y-point2.y,2));
-  },
-
-  translateBoundaries: function (boundaries,translationVector) {
-    returnBoundaries = {}
-    returnBoundaries.x1 = boundaries.x1+translationVector.x;
-    returnBoundaries.x2 = boundaries.x2+translationVector.x;
-    returnBoundaries.y1 = boundaries.y1+translationVector.y;
-    returnBoundaries.y2 = boundaries.y2+translationVector.y;
-    return returnBoundaries;
-  },
-
-  getBoundaries: function (path) {
-    var boundingBox = path.getBBox();
-    return {
-      'x1': boundingBox.x,
-      'y1': boundingBox.y,
-      'x2': boundingBox.x+boundingBox.width,
-      'y2': boundingBox.y+boundingBox.height
-    };
-  },
-
-  expand: function(orig) {
-    var out = {};
-    var expansion = 5;
-    out.x1 = orig.x1-expansion;
-    out.x2 = orig.x2+expansion;
-    out.y1 = orig.y1-expansion;
-    out.y2 = orig.y2+expansion;
-    return out;
-  },
-
-  hasOverlap: function (aOrig, bOrig) {
-
-    // Enlarge a and b
-    var a = rendererHelpers.expand(aOrig);
-    var b = rendererHelpers.expand(bOrig);
-
-    // X Overlap
-    var xOverlap = false;
-    if (a.x1 < b.x1 && a.x2 > b.x1) {
-      xOverlap = true;
-    } else if (a.x1 < b.x2 && a.x2 > b.x2) {
-      xOverlap = true;
-    } else if (b.x1 < a.x1 && b.x2 > a.x1) {
-      xOverlap = true;
-    } else if (b.x1 < a.x2 && b.x2 > a.x2) {
-      xOverlap = true;
-    }
-
-    // Y Overlap
-    var yOverlap = false;
-    if (a.y1 < b.y1 && a.y2 > b.y1) {
-      yOverlap = true;
-    } else if (a.y1 < b.y2 && a.y2 > b.y2) {
-      yOverlap = true;
-    } else if (b.y1 < a.y1 && b.y2 > a.y1) {
-      yOverlap = true;
-    } else if (b.y1 < a.y2 && b.y2 > a.y2) {
-      yOverlap = true;
-    }
-
-    if (xOverlap && yOverlap) {
-      return true;
-    } else {
-      return false;
-    }
-
-  },
-
-  addMargin: function (box, margin) {
-    box.x1 = box.x1-margin;
-    box.x2 = box.x2+margin;
-    box.y1 = box.y1-margin;
-    box.y2 = box.y2+margin;
-    return box;
-  },
-
-  getNormalVectors: function (segment) {
-
-    // Get dx and dy for the current segment
-    var totalLength = segment.getTotalLength();
-    var initialPoint = segment.getPointAtLength(0);
-    var terminalPoint = segment.getPointAtLength(totalLength);
-    var dx = terminalPoint.x - initialPoint.x;
-    var dy = terminalPoint.y - initialPoint.y;
-
-    // Normalize
-    var length = Math.sqrt(Math.pow(dx,2)+Math.pow(dy,2));
-    dx = dx/length;
-    dy = dy/length;
-
-    return [{'x':dy, 'y':-1*dx}, {'x': -1*dy, 'y':dx}]
-  },
-
-  getCentroid: function(activeSegment, roadFeature, spotlightRadius) {
-
-    // Calculate Centroid
-    var activeSegmentMidpoint = activeSegment.getPointAtLength(activeSegment.getTotalLength()/2);
-    var segments = roadFeature.selectAll('.segment')[0];
-    var cNumeratorX = 0;
-    var cNumeratorY = 0;
-    var cDenominator = 0;
-    for (var i=0; i<segments.length; i++) {
-      var segment = segments[i];
-
-      var totalLength = segment.getTotalLength();
-      var midPoint = segment.getPointAtLength(totalLength/2);
-      var epicenterDistance = rendererHelpers.calculateDistance(activeSegmentMidpoint,midPoint);
-      if (epicenterDistance < spotlightRadius) {
-        var initialPoint = segment.getPointAtLength(0);
-        var terminalPoint = segment.getPointAtLength(totalLength);
-        var xCentroid = 1/3*(initialPoint.x+midPoint.x+terminalPoint.x);
-        var yCentroid = 1/3*(initialPoint.y+midPoint.y+terminalPoint.y);
-
-        cNumeratorX += xCentroid*totalLength;
-        cNumeratorY += yCentroid*totalLength;
-        cDenominator += totalLength;
-      }
-    }
-    return {'x': cNumeratorX/cDenominator, 'y': cNumeratorY/cDenominator};
-  },
-
-  getCloserIndex: function(csm, com, offsetVectors) {
-    var closerIndex;
-    var distance, candidateDistance
-    for (var i=0; i<offsetVectors.length; i++) {
-      offsetVector = offsetVectors[i];
-      if (typeof offsetVector !== 'undefined') {
-        translatedCsm = {'x': csm.x+offsetVector.x, 'y': csm.y+offsetVector.y }
-        candidateDistance = rendererHelpers.calculateDistance(com,translatedCsm);
-        if (typeof distance == 'undefined' || candidateDistance <= distance) {
-          closerIndex = i;
-        }
-      }
-    }
-    return closerIndex;
-  },
-
-  applyNormalVectorMultiplier: function(normalVector, multiplier) {
-    offsetVector = {};
-    offsetVector.x = normalVector.x*multiplier;
-    offsetVector.y = normalVector.y*multiplier;
-    return offsetVector
-  },
-
-  getOffsetVector: function(currentSegment, queuedTransformations, currentSegmentMidpoint, com, currentBoundaries) {
-    var baseMultiplier = 7;
-    var multiplier = baseMultiplier;
-
-    // Calculate the normal vectors
-    var normalVectors = rendererHelpers.getNormalVectors(currentSegment);
-
-    // Calcuate the base offset
-    var offsetVectors = [];
-    var closerIndex = rendererHelpers.getCloserIndex(currentSegmentMidpoint, com, normalVectors);
-    offsetVectors[closerIndex] = rendererHelpers.applyNormalVectorMultiplier(normalVectors[closerIndex], multiplier)
-
-    // Check if the base offset has any collisions
-    var translatedBoundaries = rendererHelpers.translateBoundaries(currentBoundaries,offsetVectors[closerIndex]);
-    for (var j=0; j<queuedTransformations.length; j++) {
-      var previousBoundaries = queuedTransformations[j].boundaries;
-      if (rendererHelpers.hasOverlap(translatedBoundaries, previousBoundaries)) {
-        //console.log('has overlap');
-        //console.log('  previousCollision: '+JSON.stringify(queuedTransformations[j]));
-        multiplier += baseMultiplier;
-        offsetVectors[closerIndex] = rendererHelpers.applyNormalVectorMultiplier(normalVectors[closerIndex], multiplier)
-        closerIndex = rendererHelpers.getCloserIndex(currentSegmentMidpoint, com, offsetVectors);
-        translatedBoundaries = rendererHelpers.translateBoundaries(currentBoundaries,offsetVectors[closerIndex]);
-        //console.log("  translatedBoundaries: "+JSON.stringify(translatedBoundaries));
-        j = 0;
-      }
-    }
-    return offsetVectors[closerIndex];
   }
 }
 
@@ -666,7 +378,7 @@ $.when(
         if (zonePairIds.indexOf(parseInt(pairId)) !== -1) {
           pairDatum = current.pairData[parseInt(pairId)];
           if (typeof pairDatum !== 'undefined') {
-            distance = pairDatum.travelTime/60*parseInt(pairDatum.speed);
+            distance = pairDatum.travelTime/60*pairDatum.speed;
             percentiles = allGraphData.similar_dow[parseInt(pairId)];
             for (percentile in percentiles) {
               if (typeof numerator[percentile] === 'undefined') {
@@ -718,7 +430,7 @@ $.when(
           if (zonePairIds.indexOf(parseInt(pairId)) !== -1) {
             pairDatum = current.pairData[pairId];
             if (typeof pairDatum !== 'undefined') {
-              distance = pairDatum.travelTime/60*parseInt(pairDatum.speed);
+              distance = pairDatum.travelTime/60*pairDatum.speed;
               travelTimes = allGraphData[simpleItem][pairId];
               for (var i=0; i<travelTimes.length; i++) {
                 if (typeof numerator[i] === 'undefined') {
